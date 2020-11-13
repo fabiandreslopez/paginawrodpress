a8717db83be1659245aeb6f78a2929eed1ee9c25
fabiandreslopez <fabiandreslopez91@gmail.com>
2020-11-12 22:13:22 -0500

    modificacion de pagina

---
+++ wp-content/themes/siteorigin-north/inc/contributors.php
@@ -0,0 +1,52 @@
+<?php
+return json_decode( '{
+	"b7122312e4008f8f2c76911080bca01a": {
+		"name": "Andrew Misplon",
+		"email": "b7122312e4008f8f2c76911080bca01a",
+		"loc": 1723,
+		"score": 47.27746546495827,
+		"percent": 52.54408584088669
+	},
+	"85ce7a450a7ee4895f3bd823505e2e12": {
+		"name": "adiraomj",
+		"email": "85ce7a450a7ee4895f3bd823505e2e12",
+		"loc": 5257,
+		"score": 26.290878479326523,
+		"percent": 29.21963269528397
+	},
+	"5cff8d4385a469d1baab761889ad3161": {
+		"name": "Alex S",
+		"email": "5cff8d4385a469d1baab761889ad3161",
+		"loc": 256,
+		"score": 14.910508321129358,
+		"percent": 16.571510791697804
+	},
+	"0f7f79a15dafbc4c1b64dfdc79c6937d": {
+		"name": "adiraomj",
+		"email": "0f7f79a15dafbc4c1b64dfdc79c6937d",
+		"loc": 340,
+		"score": 0.7843441975207721,
+		"percent": 0.8717186600004893
+	},
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 48,
+		"score": 0.46773166930283416,
+		"percent": 0.5198360940174617
+	},
+	"d203744279876c5eda043108e2611648": {
+		"name": "Alex S",
+		"email": "d203744279876c5eda043108e2611648",
+		"loc": 12,
+		"score": 0.2431491477053515,
+		"percent": 0.27023550360663784
+	},
+	"36639e6e074b731b093bde5a77305179": {
+		"name": "Braam Genis",
+		"email": "36639e6e074b731b093bde5a77305179",
+		"loc": 2,
+		"score": 0.002681680376932418,
+		"percent": 0.002980414506945116
+	}
+}', true );
